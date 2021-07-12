80be55f35717cf6b09fd240a2a47e9c9596d163d
James Butcher <jamesbutcher167@gmail.com>
2021-07-12 22:23:01 +0100

    Remove unwanted code

--- dwmstatus.cpp
+++ dwmstatus.cpp
@@ -47,73 +47,6 @@ void settz(char *tzname){
 	setenv((char*)"TZ", tzname, 1);
 }
 
-int parse_netdev(unsigned long long int *receivedabs, unsigned long long int *sentabs){
-	char buf[255];
-	char *datastart;
-	static int bufsize;
-	int rval;
-	FILE *devfd;
-	unsigned long long int receivedacc, sentacc;
-
-	bufsize = 255;
-	devfd = fopen("/proc/net/dev", "r");
-	rval = 1;
-
-	// Ignore the first two lines of the file
-	fgets(buf, bufsize, devfd);
-	fgets(buf, bufsize, devfd);
-
-	while (fgets(buf, bufsize, devfd)) {
-	    if ((datastart = strstr(buf, "lo:")) == NULL) {
-		datastart = strstr(buf, ":");
-
-		// With thanks to the conky project at http://conky.sourceforge.net/
-		sscanf(datastart + 1, "%llu  %*d     %*d  %*d  %*d  %*d   %*d        %*d       %llu",\
-		       &receivedacc, &sentacc);
-		*receivedabs += receivedacc;
-		*sentabs += sentacc;
-		rval = 0;
-	    }
-	}
-
-	fclose(devfd);
-	return rval;
-}
-
-void calculate_speed(char *speedstr, unsigned long long int newval, unsigned long long int oldval){
-	double speed;
-	speed = (newval - oldval) / 1024.0;
-	if (speed > 1024.0) {
-	    speed /= 1024.0;
-	    sprintf(speedstr, "%.3f MB/s", speed);
-	} else {
-	    sprintf(speedstr, "%.2f KB/s", speed);
-	}
-}
-
-char * get_netusage(unsigned long long int *rec, unsigned long long int *sent){
-	unsigned long long int newrec, newsent;
-	newrec = newsent = 0;
-	char downspeedstr[15], upspeedstr[15];
-	static char retstr[42];
-	int retval;
-
-	retval = parse_netdev(&newrec, &newsent);
-	if (retval) {
-	    fprintf(stdout, "Error when parsing /proc/net/dev file.\n");
-	    exit(1);
-	}
-
-	calculate_speed(downspeedstr, newrec, *rec);
-	calculate_speed(upspeedstr, newsent, *sent);
-
-	sprintf(retstr, "down: %s up: %s", downspeedstr, upspeedstr);
-
-	*rec = newrec;
-	*sent = newsent;
-	return retstr;
-}
-
 char * mktimes(char *fmt, char *tzname){
 	char buf[129];
 	time_t tim;
@@ -225,11 +158,10 @@ int main(void){
 		tmlndn = mktimes((char*)"Date: %a %d %b | Time: %H:%M %Y", tzgmt);
 		netstats = get_netusage(&rec, &sent);
 
-		status = smprintf((char*)"Battery: %s | %s | Internet: %s",
-				bat, tmlndn, netstats);
+		status = smprintf((char*)"Battery: %s | %s",
+				bat, tmlndn);
 
 		setstatus(status);
-		free(netstats);
 		free(bat);
 		free(status);
 		free(tmlndn);
